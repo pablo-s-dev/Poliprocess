445a25cfd6a5b6f9d6a4c45ded22ec1302e8b9bd
pablo-s-dev <ps.login.username@gmail.com>
2023-11-14 11:53:55 -0300

    Layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-white">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-white dark:via-white lg:static lg:h-auto lg:w-auto lg:bg-none">
         </div>
       </div>
 
-      <div className="relative flex place-items-center">
+      <div className="relative flex place-items-center flex-col ">
         <div>
-          <h1>Poli process </h1>
+          <h1 className='p-5 text-5xl text-black'>Poli process </h1>
         </div>
         
-        <div>
+        <div className="p-10">
           <Image
             className="relative"
             src="/background_trash_can.jpg"
@@ -26,14 +26,14 @@ export default function Home() {
         
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left text-black">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`mb-3 text-2xl font-semibold text-black`}>
             Produtos{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
