726baae67004fc6e578c77e7d427da084c32c6b2
Pablo Santana <ps.login.username@gmail.com>
2025-03-07 22:29:24 -0300

    fix title link not clickable

--- src/app/produtos/_components/LogoBtn.tsx
+++ src/app/produtos/_components/LogoBtn.tsx
@@ -1,8 +1,12 @@
 export default function Logo() {
-    return (
-        <a href="/" className="flex flex-row absolute left-[2vmin] ">
-            <h1 className="text-xl text-black bg-[#10101014] p-[1vmin] rounded-lg">| Poliprocess</h1>
-        </a>
-    );
-
+  return (
+    <a
+      href="/"
+      className="flex flex-row absolute left-[2vmin] cursor-pointer z-10"
+    >
+      <h1 className="text-xl text-black bg-[#10101014] p-[1vmin] rounded-lg">
+        | Poliprocess
+      </h1>
+    </a>
+  );
 }
